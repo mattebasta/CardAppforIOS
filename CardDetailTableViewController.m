69bf17ee8c8d9fc8ca838eaf7b1d69828acce61f
mattebasta <matteobastante@gmail.com>
2021-09-25 11:04:32 +0200

    added qr code generator

--- CardDetailTableViewController.m
+++ CardDetailTableViewController.m
@@ -17,21 +17,47 @@
 
 @implementation CardDetailTableViewController
 
--(CIImage *)createQrForString:(NSString *)qrString {
-    NSData *stringData = [qrString dataUsingEncoding:NSISOLatin1StringEncoding];
+-(CIImage *)createQrForString: (NSString *)qrString{
+    NSData *stringData = [qrString dataUsingEncoding:NSUTF8StringEncoding];
     
     CIFilter *qrFilter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
-    [qrFilter setValue:stringData forKey:self.clientIdDetail];
+    [qrFilter setValue:stringData forKey:@"inputMessage"];
+    [qrFilter setValue:@"H" forKey:@"inputCorrectionLevel"];
     
     return qrFilter.outputImage;
 }
+
+-(UIImage *)createNonInterpolatedUIImageFromCIImage:(CIImage *)image withScale:(CGFloat)scale{
+    //render the CIImage into a CGImage
+    CGImageRef cgImage = [[CIContext contextWithOptions:nil] createCGImage:image fromRect:image.extent];
+    
+    //rescale using core graphics
+    UIGraphicsBeginImageContext(CGSizeMake(image.extent.size.width * scale, image.extent.size.width * scale));
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetInterpolationQuality(context, kCGInterpolationNone);
+    CGContextDrawImage(context, CGContextGetClipBoundingBox(context), cgImage);
+    
+    //get the image
+    UIImage *scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    CGImageRelease(cgImage);
+    
+    return scaledImage;
+    
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Detail";
     self.compNameLabel.text = self.compNameDetail;
     self.idCodeLabel.text = self.clientIdDetail;
-    //[self.qrCodeImageView setImage:_outputImage];
+    //Runtime Generation
+    CIImage *qrCode = [self createQrForString:_clientIdDetail];
+    UIImage *qrCodeImg = [self createNonInterpolatedUIImageFromCIImage:qrCode withScale:2*[[UIScreen mainScreen] scale]];
+    //Runtime Generation
+    self.qrCodeImageView.image = qrCodeImg;
 }
 
 
